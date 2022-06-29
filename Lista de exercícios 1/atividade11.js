ae288cc5a2e131ce3f11d96b3abd72f3a1f86722
Danz-Souza <50535257+Danz-Souza@users.noreply.github.com>
2022-06-29 19:18:19 -0300

    Update atividade11.js

--- Lista de exercícios 1/atividade11.js
+++ Lista de exercícios 1/atividade11.js
@@ -1,5 +1,4 @@
-/* Sabendo que str( ) converte valores numéricos para string, calcule quantos dígitos há em 2 elevado
-a um milhão. */
+/* converta valores numéricos para string, calcule quantos dígitos há em 2 elevado a um milhão. */
 
 let numero = 2**1000000
 let contagem = numero + ''
